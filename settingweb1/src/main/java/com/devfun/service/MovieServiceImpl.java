564237540ba04c68265bdd4c52b9ce19fd8a6f49
call203 <call203@daum.net>
2020-04-10 20:45:56 +0900

    setting spring projectv2

--- settingweb1/src/main/java/com/devfun/service/MovieServiceImpl.java
+++ settingweb1/src/main/java/com/devfun/service/MovieServiceImpl.java
@@ -1,5 +1,24 @@
 package com.devfun.service;
-
-public class MovieServiceImpl {
-
+ 
+import java.util.List;
+ 
+import javax.inject.Inject;
+ 
+import org.springframework.stereotype.Service;
+ 
+import com.devfun.dao.MovieDAO;
+import com.devfun.vo.MovieVO;
+ 
+@Service
+public class MovieServiceImpl implements MovieService {
+ 
+    @Inject
+    private MovieDAO dao;
+    
+    @Override
+    public List<MovieVO> selectMovie() throws Exception {
+ 
+        return dao.selectMovie();
+    }
+ 
 }
--- settingweb1/src/main/java/com/devfun/vo/MovieVO.java
+++ settingweb1/src/main/java/com/devfun/vo/MovieVO.java
@@ -1,5 +1,33 @@
 package com.devfun.vo;
 
 public class MovieVO {
+	private int Movie_id;
+	public int getMovie_id() {
+		return Movie_id;
+	}
+	public void setMovie_id(int movie_id) {
+		Movie_id = movie_id;
+	}
+	public String getMovie_name() {
+		return Movie_name;
+	}
+	public void setMovie_name(String movie_name) {
+		Movie_name = movie_name;
+	}
+	public String getDirector() {
+		return director;
+	}
+	public void setDirector(String director) {
+		this.director = director;
+	}
+	public String getTypes() {
+		return types;
+	}
+	public void setTypes(String types) {
+		this.types = types;
+	}
+	private String Movie_name;
+	private String director;
+	private String types;
 
 }
