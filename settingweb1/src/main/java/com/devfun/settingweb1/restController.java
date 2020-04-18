a3aeb1f9d988a0304741c7eec2393b6b62813004
call203 <call203@daum.net>
2020-04-18 17:58:26 +0900

    RestControll

---
+++ settingweb1/src/main/java/com/devfun/settingweb1/restController.java
@@ -0,0 +1,44 @@
+package com.devfun.settingweb1;
+
+import java.util.List;
+import java.util.Locale;
+ 
+import javax.inject.Inject;
+ 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.devfun.vo.MovieVO;
+import com.devfun.service.MovieService;
+ 
+/**
+ * Handles requests for the application home page.
+ */
+@RestController
+public class restController {
+    
+    private static final Logger logger = LoggerFactory.getLogger(restController.class);
+    
+    @Inject
+    private MovieService service;
+    
+    /**
+     * Simply selects the home view to render by returning its name.
+     */
+    @RequestMapping(value = "/restex")
+    public  List<MovieVO> movieList() throws Exception{
+ 
+        logger.info("home");
+        
+        List<MovieVO> movieList = service.selectMovie();
+        
+ 
+        return movieList;
+    }
+    
+}
--- settingweb1/src/main/webapp/WEB-INF/views/home.jsp
+++ settingweb1/src/main/webapp/WEB-INF/views/home.jsp
@@ -3,11 +3,16 @@
 <%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
 <html>
 <head>
+ 
 <title>Home</title>
+    <%@include file="./common_resource.jsp"%>
+    <script type="text/javascript" src="${pageContext.request.contextPath}/resources/js/home.js"></script>
+    <link href="${pageContext.request.contextPath}/resources/css/home.css" rel="stylesheet" />
+ 
 </head>
 <body>
     <h1>Hello world!</h1>
- 
+     <button id = "load_movieList" type = "button">영화 목록 가져오기</button>
     <table>
         <thead>
             <tr>
@@ -16,16 +21,20 @@
                 <th>장르</th>
             </tr>
         </thead>
-        <tbody>
-            <c:forEach items="${movieList}" var="movie">
+        <tbody id ="movieList" >
+            <!--<c:forEach items="${movieList}" var="movie">
                 <tr>
                     <td>${movie.movie_name}</td>
                     <td>${movie.director}</td>
                     <td>${movie.types}</td>
                 </tr>
-            </c:forEach>
+            </c:forEach>-->
         </tbody>
     </table>
+ 
+ 
+</body>
+</html>
  
  
 </body>
